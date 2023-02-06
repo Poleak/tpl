b0b40997eb0cc4ce28846f264b70a9d976edaa4d
Poleak THUOK <poleak.thuok@CKSNTRANSPORT.COM>
2023-02-06 12:23:02 +0700

    go

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function Check(){
+    let uname=document.getElementById("uname");
+    let pwd=document.getElementsByTagName("pwd").item;
+    console.log(uname.value);
+    console.log(uname.value);
+    if (uname.value=="poleak" && pwd.value=="poleak"){
+        alert("correct");
+    }
+    else{
+        alert("Wrong username or password.");
+    }
+}
+$(document).ready(function(){
+    const date=new Date();
+    const hour=date.getHours();
+    let message="";
+    if(hour<=12){
+        message="Good Morning!";
+    }
+    if(hour>12){
+        message="Good Afternoon!";
+    }
+    if(hour>17){
+        message="Good Evening!";
+    }
+    $('#headingText').empty().append(message + " From TPL")
+})
